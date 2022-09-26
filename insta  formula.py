c729edcf2f9e5b0dcb62d07d9071493204972944
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-09-26 15:38:00 +0300

    Create insta  formula.py

---
+++ insta  formula.py
@@ -0,0 +1,87 @@
+
+print('''
+  _  __     __      __     _   _ _____ 
+ | |/ /    /\ \    / /\   | \ | |_   _|
+ | ' /    /  \ \  / /  \  |  \| | | |  
+ |  <    / /\ \ \/ / /\ \ | . ` | | |  
+ | . \  / ____ \  / ____ \| |\  |_| |_ 
+ |_|\_\/_/    \_\/_/    \_\_| \_|_____|
+''')
+print('''
+|=================================================|
+|      [ + ] by kivani                            |
+|=================================================|
+|      [ + ] Instagram : 2knn01                   |               
+|=================================================|
+|      [ + ] Telegram : https://t.me/kavani2knn10 |                
+|=================================================|
+''')
+print('''
+==========================================
+ - prime formula         [1]\n 
+==========================================
+ - second formula        [2]\n 
+==========================================
+ - Insta support number  [3]\n
+==========================================
+''')
+choose = input("Please choose an option : ")    
+if choose == '1': 
+        print("=================================================")
+        print(" - Target information")
+        print("=================================================")
+        user = input("Username @ : ") 
+        date = input("Sign-up date : ")
+        location = input("Recent login location : ")     
+        login = input("Recent login : ")
+        phone = input("Number phone : ")
+        Email = input("Used email   : ")
+        print("=================================================")
+        print("Hey there Instagram support,I want to recover my account") 
+        print("The following information is my info ") 
+        print("Username @ : "+user)
+        print("Sign-up date : "+date)
+        print("Recent login location : "+location)      
+        print("Recent login : "+login)
+        print("Used phone : "+phone)
+        print("Email : "+Email)
+        print("I hope to recover my account as soon as possible")
+        style = '''\033[31m
+(Copy and paste it into Instagram support)       
+        '''
+        print(style)
+
+                
+if choose == '2':        
+        print("=================================================")
+        print(" - Target information")
+        print("=================================================")
+        
+        user = input("Username @ : ")
+        date = input("Sign-up date : ")
+        Location = input("Recent login location : ")
+        face = input("My Facebook : ")
+        email = input("My Email : ")
+        Number = input("Phone Number : ") 
+        print("=================================================")
+        print("Hi Instagram, I lost my account and I need to recover the account,")
+        print("here is the information about the account below")       
+        print("Username @ : "+user)
+        print("Sign-up date : "+date)
+        print("Recent login location : "+Location)
+        print("My Facebook : "+face)                   
+        print("Email : "+email)
+        print("phone number : "+Number) 
+        print("I understand that")
+        style = '''\033[31m
+(Copy and paste it into Instagram support)       
+        '''
+        print(style)
+        
+        
+if choose == '3':
+
+        print("Instagram support number only WhatsApp")
+        print("US number") 
+        print("+1(650)521-2669 ")
+        
